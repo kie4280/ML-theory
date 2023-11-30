5dc37672dc0ebf4905393cc9b643aa1cd697a251
kie4280 <kie4280@livemail.tw>
2023-11-30 14:07:10 +0800

    added comments

--- hw5/GP.py
+++ hw5/GP.py
@@ -5,6 +5,9 @@ from typing import Tuple
 
 
 def read_data(data_loc: str = "./data/input.data") -> np.ndarray:
+    """
+    Read in data from the file specified by data_loc
+    """
     data = None
     with open(data_loc) as f:
         data = f.readlines()
@@ -19,10 +22,13 @@ def read_data(data_loc: str = "./data/input.data") -> np.ndarray:
 
 def make_kernel(data: np.ndarray, length: float,
                 scale_mixture: float):
+    """
+    Take the x component of data and make the rational quadratic kernel
+    """
     x = data
     n = x.shape[0]
-    x = np.broadcast_to(x, (n, n))
-    K = (1 + ((x - x.T)**2) /
+    x = np.broadcast_to(x, (n, n)) # broadcast the vector into an nxn array
+    K = (1 + ((x - x.T)**2) / # do an element-wise subtraction and apply the rational quadratic kernel
                (2 * scale_mixture * length * length))**(-scale_mixture)
 
     return K
@@ -35,8 +41,10 @@ def GP(params: Tuple,
        visualization: bool = False):
     length, scale_mixture = params
     # print(var, length, scale_mixture, data, beta, points, visualization)
-    x_plot = np.linspace(-60, 60, points)
+    x_plot = np.linspace(-60, 60, points) # this is the testing data
     N = data.shape[0]
+    # we will be concatenating the training and testing data and feed it into the 
+    # make_kernel function for faster computation
     new_d = np.concatenate([data[:, 0], x_plot], axis=0)
     C = make_kernel(new_d, length,
                     scale_mixture) + 1 / beta * np.identity(N + points)
@@ -44,6 +52,7 @@ def GP(params: Tuple,
     means = []
     vars = []
     C_inv = np.linalg.inv(C[:N, :N])
+    # calculating the mean and variance for each testing data and putting them in a list
     for i in range(points):
         k = C[N + i, :N]
         m = k.T @ C_inv @ y
@@ -55,12 +64,16 @@ def GP(params: Tuple,
     std = np.sqrt(np.array(vars))
     if visualization:
         plt.figure(dpi=300)
+        # fill be 95th percentile confidence area
         plt.fill_between(x_plot, means - 2 * std, means + 2 * std)
         plt.plot(x_plot, means, c="green")
         plt.scatter(data[:, 0], data[:, 1], marker='o', c="red")
         plt.show()
 
+    # C_N is just the covariance matrix of the training data
     C_N = C[:N, :N]
+    # returning the marginal negative log-likelihood to compute the loss to be fed into 
+    # scipy.optimize.minimize
     return -1 / 2 * np.log(np.linalg.det(
         C_N)) - 1 / 2 * y.T @ C_inv @ y - N / 2 * np.log(2 * np.pi)
 
